443d2c4e16842f0ef73005da372561ad09d5d387
waggyes <sergeyvagapov@gmail.com>
2021-09-18 13:15:23 +0700

    After prettier

--- contracts/AuctionStorage.sol
+++ contracts/AuctionStorage.sol
@@ -1,25 +1,32 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-import '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
-import '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 
-import '@openzeppelin/contracts/access/Ownable.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 // Contract to accept, storage and withdraw ETH, and storage and transfer tokens
 
 contract AuctionStorage is Ownable, ERC1155Holder {
+    mapping(address => mapping(address => mapping(uint256 => uint256)))
+        public assets; // user address => token address => token id => amount
+    mapping(uint256 => mapping(address => uint256)) public deposits; // auction id => user address => bid's hash
 
-    mapping(address => mapping(address => mapping(uint256 => uint256 ))) public assets;                // user address => token address => token id => amount
-    mapping(uint256 => mapping(address => uint256)) public deposits;                                   // auction id => user address => bid's hash
+    uint256 profit; // how much owner can withdraw
 
-    uint256 profit;                                                                             // how much owner can withdraw
-
-    function _setBidDeposit(uint256 _auctionId, address _to, uint256 _amount) internal {
+    function _setBidDeposit(
+        uint256 _auctionId,
+        address _to,
+        uint256 _amount
+    ) internal {
         deposits[_auctionId][_to] = _amount;
     }
 
-    function _transferETH(address payable _receiver, uint256 _amount) internal returns(bool){
+    function _transferETH(address payable _receiver, uint256 _amount)
+        internal
+        returns (bool)
+    {
         (bool success, ) = _receiver.call{value: _amount, gas: 2300}("");
         return success;
     }
@@ -30,24 +37,45 @@ contract AuctionStorage is Ownable, ERC1155Holder {
         require(deposit > 0, "zero deposit");
         deposits[_auctionId][sender] = 0;
         _transferETH(payable(sender), deposit);
-    } 
+    }
 
     function takeProfit() external onlyOwner {
         uint256 balance = profit;
-        require(balance > 0, 'zero balance');
+        require(balance > 0, "zero balance");
         bool success = _transferETH(payable(_msgSender()), balance);
         profit = 0;
         require(success, "!takeProfit");
     }
 
-    function _transferAssets(address recepient, uint256 _amount, address _token, uint256 _tokenId) internal {
-        require(assets[recepient][_token][_tokenId] >= _amount, 'not enough tokens');
+    function _transferAssets(
+        address recepient,
+        uint256 _amount,
+        address _token,
+        uint256 _tokenId
+    ) internal {
+        require(
+            assets[recepient][_token][_tokenId] >= _amount,
+            "not enough tokens"
+        );
         assets[recepient][_token][_tokenId] -= _amount;
-        IERC1155(_token).safeTransferFrom(address(this), recepient, _tokenId, _amount, '');
+        IERC1155(_token).safeTransferFrom(
+            address(this),
+            recepient,
+            _tokenId,
+            _amount,
+            ""
+        );
     }
 
-    function _makeExchange(uint256 _auctionId, address _token, uint256 _tokenId, address[] memory _winners, uint256[] memory _prices, uint256[] memory _amounts) internal {
-        uint256 sumETH; 
+    function _makeExchange(
+        uint256 _auctionId,
+        address _token,
+        uint256 _tokenId,
+        address[] memory _winners,
+        uint256[] memory _prices,
+        uint256[] memory _amounts
+    ) internal {
+        uint256 sumETH;
         for (uint256 i = 0; i < _winners.length; i++) {
             sumETH += _prices[i];
             deposits[_auctionId][_winners[i]] -= _prices[i];
@@ -55,6 +83,4 @@ contract AuctionStorage is Ownable, ERC1155Holder {
         }
         profit += sumETH;
     }
-
-
 }
--- contracts/VCG.sol
+++ contracts/VCG.sol
@@ -1,15 +1,13 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.7;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
-import './AuctionStorage.sol';
-import 'hardhat/console.sol';
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "./AuctionStorage.sol";
+import "hardhat/console.sol";
 
- 
 contract VCG is AuctionStorage {
-
-    mapping(uint256 => mapping(address => bytes)) public bidHashs;       // auction id => user address => bid's hash
+    mapping(uint256 => mapping(address => bytes)) public bidHashs; // auction id => user address => bid's hash
     mapping(uint256 => AuctionInfo) public auctions;
     struct AuctionInfo {
         uint256 id;
@@ -24,9 +22,9 @@ contract VCG is AuctionStorage {
 
     event AuctionStarted(
         uint256 id,
-        uint256 start, 
+        uint256 start,
         address tokenToSale,
-        uint256 tokenIdToSale, 
+        uint256 tokenIdToSale,
         uint256 amountToSale
     );
 
@@ -49,11 +47,17 @@ contract VCG is AuctionStorage {
         uint256 _tokenIdToSale,
         uint256 _amountToSale
     ) external onlyOwner {
-        require(_tokenToSale != address(0), 'token is 0');
-        require(_amountToSale > 0, 'amount is 0');
+        require(_tokenToSale != address(0), "token is 0");
+        require(_amountToSale > 0, "amount is 0");
         AuctionInfo memory newAuction;
         newAuction.tokenToSale = _tokenToSale;
-        IERC1155(_tokenToSale).safeTransferFrom(_msgSender(), address(this), _tokenIdToSale, _amountToSale, "");
+        IERC1155(_tokenToSale).safeTransferFrom(
+            _msgSender(),
+            address(this),
+            _tokenIdToSale,
+            _amountToSale,
+            ""
+        );
         newAuction.startAuction = block.timestamp;
         newAuction.tokenIdToSale = _tokenIdToSale;
         newAuction.amountToSale = _amountToSale;
@@ -62,15 +66,24 @@ contract VCG is AuctionStorage {
         newAuction.id = newId;
         newAuction.active = true;
         auctions[newId] = newAuction;
-        emit AuctionStarted(newId, newAuction.startAuction, _tokenToSale, _tokenIdToSale, _amountToSale);
+        emit AuctionStarted(
+            newId,
+            newAuction.startAuction,
+            _tokenToSale,
+            _tokenIdToSale,
+            _amountToSale
+        );
     }
 
-    function createBid(uint256 _auctionId, bytes memory _hash) external payable {
+    function createBid(uint256 _auctionId, bytes memory _hash)
+        external
+        payable
+    {
         require(auctionsAmount >= _auctionId, "not exists");
-        require( auctions[_auctionId].active , "not active");
+        require(auctions[_auctionId].active, "not active");
         uint256 value = msg.value;
         address sender = _msgSender();
-        require(_hash.length != 0, 'zero hash');
+        require(_hash.length != 0, "zero hash");
         require(bidHashs[_auctionId][sender].length == 0, "bid already made");
         require(sender != owner(), "bidder cannot be owner");
         require(value > 0, "not enough deposit");
@@ -79,29 +92,55 @@ contract VCG is AuctionStorage {
         emit NewBid(_auctionId, sender, _hash, value);
     }
 
-    function claim(uint256 _auctionId, address _token, uint256 _tokenId, uint256 _amount) external {  //TODO nonReentrancy
+    function claim(
+        uint256 _auctionId,
+        address _token,
+        uint256 _tokenId,
+        uint256 _amount
+    ) external {
+        //TODO nonReentrancy
         require(auctionsAmount >= _auctionId, "not exists");
-        require(!auctions[_auctionId].active, 'not finished');
+        require(!auctions[_auctionId].active, "not finished");
         _transferAssets(_msgSender(), _amount, _token, _tokenId);
     }
 
     function returnDeposit(uint256 _auctionId) external {
         require(auctionsAmount >= _auctionId, "not exists");
-        require(!auctions[_auctionId].active, 'not finished');
+        require(!auctions[_auctionId].active, "not finished");
         _returnDeposit(_auctionId);
     }
 
     //function claimBatch
 
-    function finishAuction(uint256 _auctionId, address[] memory _winners, uint256[] memory _prices, uint256[] memory _amounts) external onlyOwner {
+    function finishAuction(
+        uint256 _auctionId,
+        address[] memory _winners,
+        uint256[] memory _prices,
+        uint256[] memory _amounts
+    ) external onlyOwner {
         require(auctionsAmount >= _auctionId, "not exists");
-        require( auctions[_auctionId].active, "already finished");
-        require(_winners.length == _prices.length && _amounts.length == _prices.length, 'incorrect data');
-        _makeExchange(_auctionId, auctions[_auctionId].tokenToSale, auctions[_auctionId].tokenIdToSale, _winners, _prices, _amounts);
-        auctions[_auctionId].active = false;        
-        emit AuctionFinished(_auctionId, auctions[_auctionId].amountToSale, _winners, block.timestamp);
+        require(auctions[_auctionId].active, "already finished");
+        require(
+            _winners.length == _prices.length &&
+                _amounts.length == _prices.length,
+            "incorrect data"
+        );
+        _makeExchange(
+            _auctionId,
+            auctions[_auctionId].tokenToSale,
+            auctions[_auctionId].tokenIdToSale,
+            _winners,
+            _prices,
+            _amounts
+        );
+        auctions[_auctionId].active = false;
+        emit AuctionFinished(
+            _auctionId,
+            auctions[_auctionId].amountToSale,
+            _winners,
+            block.timestamp
+        );
     }
 
     //TODO Only operator
-
 }
